df6af949347ccc0b7cd673c19c46c9955f52a2b0
Jeremy Live <jeremylive096@gmail.com>
2021-10-08 19:27:05 -0600

    Libreria Video + Config Module

---
+++ website/README.txt
@@ -0,0 +1,8 @@
+ionic cordova plugin add cordova-plugin-splashscreen
+npm install @ionic-native/splash-screen
+
+ionic cordova plugin add cordova-plugin-statusbar
+npm install @ionic-native/status-bar
+
+ionic cordova plugin add https://github.com/moust/cordova-plugin-videoplayer.git
+npm install @ionic-native/video-player
--- website/src/app/app.module.ts
+++ website/src/app/app.module.ts
@@ -4,6 +4,11 @@ import { RouteReuseStrategy } from '@angular/router';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { VideoPlayer } from '@ionic-native/video-player/ngx';
+
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -36,7 +41,11 @@ import { environment } from '../environments/environment';
     //, AngularFireStorageModule
 
   ],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    VideoPlayer,
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
