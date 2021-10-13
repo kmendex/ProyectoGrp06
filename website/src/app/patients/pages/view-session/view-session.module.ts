278dcbb6bc2e5e485c280eef187783ba5cb68dfb
Jeremy Live <jeremylive096@gmail.com>
2021-10-13 13:32:12 -0600

    FIX! 100% funcional!
    Sirve bien ahora si, el ver las sesiones.

--- website/src/app/patients/pages/view-session/view-session.module.ts
+++ website/src/app/patients/pages/view-session/view-session.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -13,6 +13,7 @@ import { ViewSessionPage } from './view-session.page';
     CommonModule,
     FormsModule,
     IonicModule,
+    ReactiveFormsModule,
     ViewSessionPageRoutingModule
   ],
   declarations: [ViewSessionPage]
